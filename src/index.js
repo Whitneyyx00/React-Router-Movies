b57c1e014955236cfafe76521a3d2694c5c21c74
Whitneyy <whitneyyx77@gmail.com>
2024-08-15 07:16:17 -0400

    Completed all requirements

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Import BrowserRouter from react-router-dom
+import { BrowserRouter } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // You'll need to wrap <App /> for routing to work
 root.render(
-  <App />
+  <BrowserRouter>
+     <App />
+  </BrowserRouter>
 );
